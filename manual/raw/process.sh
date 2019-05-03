d763f25e4572d6bfea79ddfd418c678e774a15cd
daveriesz <dave@riesz.net>
2019-05-03 07:03:29 -0700

    added raw text page copy and processing script

---
+++ manual/raw/process.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for x in *.txt ; do
+  y=`echo $x | sed "s/\.txt//g"`
+  cat "$x" \
+  | sed "1,/^${y}$/d" \
+  | sed '/^RELATED PAGES$/,$d' \
+  | sed "s/^  *//g" \
+  | sed "s/^Location[: ] */Location: /g" \
+  | sed "s/^\(Listed\) in \(the National Register of Historic Places\)/\1 on \2/g" \
+  | sed "s/^N[0o]\. */NO. /g" \
+  | sed "s/^\(NO\)/}\1/g" \
+  | sed "s/^\(Location.*\)$/{\1/g" \
+  | sed "s/^\(Listed.*\)$/{\1/g" \
+  | sed "s/^\(USGS.*\)$/{\1/g" \
+  | sed "s/^\(Plaque.*\)$/{\1/g" \
+  | ( tr -d "\n" && echo "" ) \
+  | tr "}" "\n" \
+  | sed "s/{/|/g" \
+  | sed "/^ *$/d" \
+  > "../$x"
+done
